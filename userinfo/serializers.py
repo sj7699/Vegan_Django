6c2fe4f247e4dbc180798a0ac500a18bcf1b7c81
sj7699 <tkdwo7699@gmail.com>
2022-06-04 21:52:14 +0900

    회원가입 serializer 비밀번호 이메일

--- userinfo/serializers.py
+++ userinfo/serializers.py
@@ -1,6 +1,19 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
 from . models import *
-
+from rest_framework.authtoken.models import Token
+from rest_framwork.validators import UniqueValidator
+class RegisterSerializer(serializers.ModelSerializer):
+    email=serializers.EmailField(
+        required=True,
+        validators=[UniqueValidator(queryset=User.objects.all())],
+    )
+    password=serializers.CharField(
+        write_only=True,
+        required=True,
+        validators=[validate_password],
+    )
 class Favorcategoryserializers(serializers.ModelSerializer):
     class Meta:
         model=Favor_Category
