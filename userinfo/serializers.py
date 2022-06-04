cc9b8be53a724b6eff30177c9936b736fcc20836
sj7699 <tkdwo7699@gmail.com>
2022-06-04 21:26:13 +0900

    시리얼라이저 수정

--- userinfo/serializers.py
+++ userinfo/serializers.py
@@ -5,4 +5,33 @@ class Favorcategoryserializers(serializers.ModelSerializer):
     class Meta:
         model=Favor_Category
         fields='__all__'
-    
+
+class Allergyserializers(serializers.ModelSerializer):
+    class Meta:
+        model=Allergy
+        fields='__all__'
+        
+class Daily_Mealserializers(serializers.ModelSerializer):
+    class Meta:
+        model=Daily_Meal
+        fields='__all__'
+
+class Productserializers(serializers.ModelSerializer):
+    class Meta:
+        model=Product
+        fields='__all__'
+
+class MEAL_PRODUCTserializers(serializers.ModelSerializer):
+    class Meta:
+        model=MEAL_PRODUCT
+        fields='__all__'
+
+class Ingredient_Detailserializers(serializers.ModelSerializer):
+    class Meta:
+        model=Ingredient_Detail
+        fields='__all__'
+
+class Product_Ingredientserializers(serializers.ModelSerializer):
+    class Meta:
+        model=Product_Ingredient
+        fields='__all__'
