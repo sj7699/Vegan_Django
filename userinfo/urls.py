c482b2dad43904e0d94a2110ef18b5c3a10a6a66
sj7699 <tkdwo7699@gmail.com>
2022-06-07 13:13:46 +0900

    url 수정

--- userinfo/urls.py
+++ userinfo/urls.py
@@ -6,6 +6,8 @@ router=DefaultRouter()
 router.register(r'favor',views.FavorcategorySet)
 router.register(r'product',views.ProductSet)
 router.register(r'rproduct',views.rlist)
+router.register(r'meal_by_client',views.meal_by_client)
+router.register(r'cut_by_price',views.cut_by_price)
 urlpatterns=[
     path('',include(router.urls)),
 ]
