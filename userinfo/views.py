5956041e0daa8f112c6b2f3be5bb15c03647a757
sj7699 <tkdwo7699@gmail.com>
2022-06-08 00:57:33 +0900

    userdetail view 수정

--- userinfo/views.py
+++ userinfo/views.py
@@ -1,10 +1,10 @@
 from datetime import date
 from itertools import product
 from django.forms import ValidationError
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
-from userinfo.models import Favor_Category,Product,Daily_Meal
+from userinfo.models import *
 from .serializers import *
 from django.db.models import Q
 from rest_framework import viewsets,generics,exceptions
@@ -16,21 +16,6 @@ from rest_framework.permissions import IsAuthenticated
 from dj_rest_auth.jwt_auth import JWTCookieAuthentication
 from django.utils import timezone
 import random
-class FavorcategorySet(viewsets.ModelViewSet):
-    queryset=Favor_Category.objects.all()
-    serializer_class = Favorcategoryserializers
-    @action(detail=False)
-    def public_list(self, request):
-        qs = self.queryset.filter(is_public=True)
-        serializer = self.get_serializer(qs, many=True)
-        return Response(serializer.data)
-    @action(detail=True, methods=['patch'])
-    def set_public(self, request, pk):
-        instance = self.get_object()
-        instance.is_public = True
-        instance.save()
-        serializer = self.get_serializer(instance)
-        return Response(serializer.data)
 
 class Daily_MealSet(viewsets.ReadOnlyModelViewSet):
     queryset=Daily_Meal.objects.all()
@@ -39,6 +24,27 @@ class Daily_MealSet(viewsets.ReadOnlyModelViewSet):
 class Meal_ProductSet(viewsets.ReadOnlyModelViewSet):
     queryset=MEAL_PRODUCT.objects.all()
     serializer_class = MEAL_PRODUCTserializers
+
+class User_DetailSet(viewsets.ModelViewSet):
+    queryset=User_Detail.objects.all()
+    serializer_calss=User_Detailserializers
+    def get_queryset(self):
+        qs=get_object_or_404(self.queryset,user=self.request.user)
+        serializer = self.get_serializer(qs,many=True)
+        return Response(serializer.data)
+    @action(detail=False,methods=['post'])
+    def modify_user(self):
+        nowuser=self.request.user
+        moduser=self.request.data
+        qs=get_object_or_404(self.queryset,user=nowuser)
+        qs.exercise = moduser.get('exercise',qs.exercise)
+        qs.gender=moduser.get('gender',qs.gender)
+        qs.height=moduser.get('height',qs.gender)
+        qs.weight=moduser.get('weight',qs.weight)
+        qs.vegan_option=moduser.get('vegan_option',qs.vegan_option)
+        qs.allergy=moduser.get('allergy',qs.allergy)
+        qs.favor_category=moduser.get('favor_category',qs.favor_category)
+        qs.avoid_category=moduser.get('avoid_category',qs.avoid_category)
 
 class ProductSet(viewsets.ModelViewSet):
     queryset=Product.objects.all()
@@ -184,42 +190,4 @@ class meal_by_client(viewsets.ModelViewSet):
             rqs.append(nowp)
         return rqs
 
-class rlist(viewsets.ReadOnlyModelViewSet):
-    queryset=Product.objects.all().order_by('?')
-    serializer_class=Productserializers
-    authentication_classes = [JWTAuthentication, BasicAuthentication, SessionAuthentication]
-    permission_classes = [IsAuthenticated]
-    def get_queryset(self):
-        calory=self.request.query_params.get('calory',None)
-        rqs=self.queryset.order_by('?')
-        if not calory.isdigit():
-            raise exceptions.ParseError("칼로리는 숫자만")
-        wtime=self.request.query_params.get('time',None)
-        print(wtime)
-        if(wtime is None):
-            raise exceptions.ParseError("식사할 시간을 입력해주세요 예) 아침 점심 저녁")
-        if(wtime!="아침" and wtime!="점심" and wtime!="저녁"):
-            raise exceptions.ParseError("식사할 시간을 정확히 입력해주세요 예) 아침 점심 저녁")
-        calory=float(calory)
-        c=0
-        cutcqs=[]
-        for x in rqs:
-            nowc=x.calory
-            if(nowc+c>calory):
-                break
-            c+=nowc
-            cutcqs.append(x)
-        if(wtime=="아침"):
-            mid=Daily_Meal.objects.create(user=self.request.user,morning=True)
-            for x in cutcqs:
-                MEAL_PRODUCT.objects.create(meal_id=mid,product_id=x)
-        if(wtime=="점심"):
-            mid=Daily_Meal.objects.create(user=self.request.user,lunch=True)
-            for x in cutcqs:
-                MEAL_PRODUCT.objects.create(meal_id=mid,product_id=x)
-        if(wtime=="점심"):
-            mid=Daily_Meal.objects.create(user=self.request.user,dinner=True)
-            for x in cutcqs:
-                MEAL_PRODUCT.objects.create(meal_id=mid,product_id=x)
-        return cutcqs
 # Create your views here.
