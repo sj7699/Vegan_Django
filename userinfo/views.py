8ba12f39a1db14a34d2bbeec68f3e238cfa6e6a5
sj7699 <tkdwo7699@gmail.com>
2022-06-07 13:08:37 +0900

    가격제한 품목선택하고 식단추가

--- userinfo/views.py
+++ userinfo/views.py
@@ -1,8 +1,10 @@
+from datetime import date
+from itertools import product
 from django.forms import ValidationError
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
-from userinfo.models import Favor_Category,Product
+from userinfo.models import Favor_Category,Product,Daily_Meal
 from .serializers import *
 from rest_framework import viewsets,generics
 from rest_framework.decorators import action
@@ -10,6 +12,7 @@ from rest_framework.response import Response
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication ,TokenAuthentication
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticated
+from django.utils import timezone
 import random
 class FavorcategorySet(viewsets.ModelViewSet):
     queryset=Favor_Category.objects.all()
@@ -69,16 +72,71 @@ class ProductSet(viewsets.ModelViewSet):
         serializer= self.get_serializer(qs,many=True)
         return Response(serializer.data)
 
-class rlist(viewsets.ReadOnlyModelViewSet):
+class cut_by_price(viewsets.ReadOnlyModelViewSet):
     queryset=Product.objects.all().order_by('?')
     serializer_class=Productserializers
     authentication_classes = [JWTAuthentication, BasicAuthentication, SessionAuthentication]
     permission_classes = [IsAuthenticated]
     def get_queryset(self):
         print(self.request.user.token)
-        calory=self.request.query_params.get('calory',None)
+        price=self.request.query_params.get('price',None)
         rqs=self.queryset.order_by('?')
         print(self.request.user.id)
+        if not price.isdigit():
+            raise ValidationError("가격은 숫자만")
+        price=int(price)
+        c=0
+        cutcqs=[]
+        for x in rqs:
+            nowc=x.price
+            if(nowc+c>price):
+                break
+            c+=nowc
+            cutcqs.append(x)
+        return cutcqs
+
+class meal_by_client(viewsets.ModelViewSet):
+    queryset=Product.objects.all()
+    serializer_class=Productserializers
+    authentication_classes = [JWTAuthentication, BasicAuthentication, SessionAuthentication]
+    permission_classes = [IsAuthenticated]
+    def get_queryset(self):
+        rqs=[]
+        product_list=self.request.data
+        for x in product_list:
+            meal_list= Daily_Meal.objects.filter(created_at__date=timezone.now().date())
+            if(not self.queryset.filter(id=x.id).exists()):
+                raise ValidationError("존재하지않는 품목입니다")
+            if(x.wtime!="아침" and x.wtime!="점심" and x.wtime!="저녁"):
+                raise ValidationError("아침 점심 저녁을 선택해주세요")
+            meal_id=1
+            if(x.wtime=="아침"):
+                if(not meal_list.filter(morning=True).exists()):
+                    meal_id=Daily_Meal.objects.create(user=self.request.user,morning=True)
+                else:
+                    meal_id=meal_list.filter(morning=True)[0].id
+            if(x.wtime=="점심"):
+                if(not meal_list.filter(lunch=True).exists()):
+                    meal_id=Daily_Meal.objects.create(user=self.request.user,lunch=True)
+                else:
+                    meal_id=meal_list.filter(lunch=True)[0].id
+            if(x.wtime=="저녁"):
+                if(not meal_list.filter(dinner=True).exists()):
+                    meal_id=Daily_Meal.objects.create(user=self.request.user,dinner=True)
+                else:
+                    meal_id=meal_list.filter(dinner=True)[0].id
+            rqs.append(x)
+            MEAL_PRODUCT.create(meal_id=meal_id,product_id=x)
+        return rqs
+
+class rlist(viewsets.ReadOnlyModelViewSet):
+    queryset=Product.objects.all().order_by('?')
+    serializer_class=Productserializers
+    authentication_classes = [JWTAuthentication, BasicAuthentication, SessionAuthentication]
+    permission_classes = [IsAuthenticated]
+    def get_queryset(self):
+        calory=self.request.query_params.get('calory',None)
+        rqs=self.queryset.order_by('?')
         if not calory.isdigit():
             raise ValidationError("칼로리는 숫자만")
         wtime=self.request.query_params.get('time',None)
